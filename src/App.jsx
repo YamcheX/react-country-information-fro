23affe590feb487bf7f84e17017fd054024fa39d
YamcheX <yamna.cherrat@gmail.com>
2025-08-27 12:19:22 +0200

    eerste opzet opdracht en helperfunctie gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css';
-
+import axios from "axios";
+import {useState} from "react";
+// endpoint info alle landen: https://restcountries.com/v3.1/independent?status=true
 function App() {
+    const [countryName, setCountryName] = useState('');
+    const [countryPopulation, setCountryPopulation] = useState(0);
+    const [countryFlag, setCountryFlag] = useState('')
+
+    async function fetchCountry (){
+        try {
+            const response = await axios.get('https://restcountries.com/v3.1/independent?status=true');
+            console.log(response.data[0].flags);
+            setCountryName(response.data[0].name.official);
+            setCountryPopulation(response.data[0].population);
+            setCountryFlag(response.data[0].flags.png);
+        } catch (e){
+            console.error(e);
+        }
+    }
 
     return (
         <>
-            Maak je applicatie hier!
+        <h1>World Regions</h1>
+            <ul>
+                <li>{countryName}</li>
+                <p>Has a population of {countryPopulation} of people</p>
+                <img src={countryFlag} alt="country flag"/>
+            </ul>
+            <button type='button' onClick={fetchCountry}>Haal de info op</button>
         </>
     )
 }
