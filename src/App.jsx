35fa274f03c2a5dae8f655903fdb41dbb1cad402
YamcheX <yamna.cherrat@gmail.com>
2025-08-29 12:12:10 +0200

    state aangepast

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css';
 import axios from "axios";
 import {useState} from "react";
+import regionColor from "./helpers/regionColor.js";
+
 // endpoint info alle landen: https://restcountries.com/v3.1/independent?status=true
 function App() {
-    const [countryName, setCountryName] = useState('');
-    const [countryPopulation, setCountryPopulation] = useState(0);
-    const [countryFlag, setCountryFlag] = useState('')
+    console.log(regionColor('Asia'));
+
+    const [countriesInfo, setCountriesInfo] = useState({
+        name: '',
+        population: 0,
+        flag: '',
+        region: '',
+    });
 
     async function fetchCountry (){
         try {
             const response = await axios.get('https://restcountries.com/v3.1/independent?status=true');
             console.log(response.data[0].flags);
-            setCountryName(response.data[0].name.official);
-            setCountryPopulation(response.data[0].population);
-            setCountryFlag(response.data[0].flags.png);
+            setCountriesInfo({
+                name: response.data[0].name.official,
+                population: response.data[0].population,
+                flag: response.data[0].flags.png,
+                region: response.data[0].region,
+            });
+            console.log(response.data[0].region);
+
         } catch (e){
             console.error(e);
         }
@@ -23,9 +35,9 @@ function App() {
         <>
         <h1>World Regions</h1>
             <ul>
-                <li>{countryName}</li>
-                <p>Has a population of {countryPopulation} of people</p>
-                <img src={countryFlag} alt="country flag"/>
+                <li>{countriesInfo.name}</li>
+                <p>Has a population of {countriesInfo.population} of people</p>
+                <img src={countriesInfo.flag} alt="country flag"/>
             </ul>
             <button type='button' onClick={fetchCountry}>Haal de info op</button>
         </>
